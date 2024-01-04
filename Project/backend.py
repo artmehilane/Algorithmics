d2ea3494289f6830256aa061c13ca5aea193f3f5
artmehilane <124361380+artmehilane@users.noreply.github.com>
2024-01-04 19:15:32 +0200

    Add files via upload

--- Project/backend.py
+++ Project/backend.py
@@ -18,6 +18,7 @@ eel.start('main.html')
 
 contacts_file = 'contacts.json'
 
+@eel.expose
 def load_contacts():
     try:
         with open(contacts_file, 'r') as file:
@@ -25,31 +26,28 @@ def load_contacts():
     except FileNotFoundError:
         return []
 
+@eel.expose
 def save_contacts(contacts):
     with open(contacts_file, 'w') as file:
         json.dump(contacts, file, indent = 4)
 
-def search_contacts(contacts, search_query):
+@eel.expose
+def search_contacts(search_query):
+    contacts = load_contacts()
     return [contact for contact in contacts if search_query.lower() in contact['name'].lower()]
 
+@eel.expose
 def sort_contacts(contacts,sort_key = 'name'):
     return sorted(contacts, key = lambda contact: contact.get(sort_key, '').lower())
 
-def add_contact(contacts, name, phone, email, address):
+@eel.expose
+def add_contact(name, phone, email, address):
+    contacts = load_contacts()
     contacts.append({"name": name, "phone": phone,  "email": email, "address": address})
     save_contacts(contacts)
 
 
-def main():
-    contacts = load_contacts()
-
-    add_contact(contacts,  "Mart Herilane", "74065663", "mart.herilane@outlook.com", "Vase 22")
-
-    print("Search Results for 'Mart':")
-    print(search_contacts(contacts, "Mart"))
-
-    print("Contacts sorted by name:")
-    print(sort_contacts(contacts))
+eel.init('frontend')
 
 if __name__ == "__main__":
-    main()
+    eel.start('main.html')
